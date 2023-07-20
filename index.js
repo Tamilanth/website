1c383eedf3691bfe6ed62f5606c3c790013191bd
Tamilanth <tamilanth@protonmail.com>
2023-07-20 19:47:39 +0530

    Add darkmode button invertion of colors when clicked

--- index.js
+++ index.js
@@ -1,58 +1,168 @@
 window.onload = () => {
-    const body = document.getElementById("root");
-    const header = document.getElementById("header");
-    const themeButton = document.getElementById("theme");
-    let darkmode = false;
-    let i = 0, j = false;
-    function animate_to_dark()
-    {
-	i++;
-	j = true;
-	body.style.color = `rgba(255, 255, 255, ${i/100})`;
-	body.style.background = `rgba(0, 0, 0, ${i/100})`;
-	header.style.background = `rgba(0, 0, 0, ${i/100 - 0.05})`;
-	themeButton.style.opacity = `${i/100}`;
-	if (i == 100)
+	const body = document.getElementById("root");
+	const header = document.getElementById("header");
+    const main = document.getElementsByTagName("main");
+	const menuIcon = document.getElementById("menuIcon");
+	const themeButton = document.getElementById("theme");
+	const menu = document.getElementById("menu");
+
+	menu.style.left = toString(window.innerWidth - 1000) + "px";
+
+
+	window.requestAnimationFrame = window.requestAnimationFrame
+		|| window.mozRequestAnimationFrame
+		|| window.webkitRequestanimationframe
+		|| window.msRequestAnimationFrame
+		|| function (f) { return setTimeout(f, 1000 / 60) } // simulate calling code 60
+
+	window.cancelAnimationFrame = window.cancelAnimationFrame
+		|| window.mozCancelAnimationFrame
+		|| function (requestID) { clearTimeout(requestID) } //fall back
+
+
+
 	{
-	    clearInterval(b);
-	    j = false;
-	    i = 0;
-	}
 
-    }
+		let i = 0, animation_id = 0;
+		let darkmode = false;
+		let is_incomplete = false;
 
-    function animate_to_light()
-    {
-	i++;
-	j = true;
-	body.style.color = `rgba(0, 0, 0, ${i/100})`;
-	body.style.background = `rgba(255, 255, 255, ${i/100})`;
-	header.style.background = `rgba(255, 255, 255, ${i/100 - 0.05})`
-	if (i == 100)
-	{
-	    clearInterval(b);
-	    j = false;
-	    i = 0;
-	}
+		function animate_to_dark() {
 
-    }
-   
-    themeButton.addEventListener("click", () => {
-	if (j)
-	    clearInterval(b);
+			i++;
+			is_incomplete = true;
+			body.style.color = `rgba(255, 255, 255, ${i / 100})`;
+			body.style.background = `rgba(0, 0, 0, ${i / 100})`;
+			header.style.background = `rgba(0, 0, 0, ${i / 100 - 0.15})`;
+			themeButton.style.filter = `invert(${i}%)`;
+			themeButton.style.opacity = `${i / 100}`;
+			if (i >= 100) {
+				window.cancelAnimationFrame(animation_id);
+				is_incomplete = false;
+			    i = 0;
+			    return;
+			}
+		    animation_id = window.requestAnimationFrame(animate_to_dark);
+
+		}
+
+		function animate_to_light() {
+			i++;
+			is_incomplete = true;
+			body.style.color = `rgba(0, 0, 0, ${i / 100})`;
+			body.style.background = `rgba(255, 255, 255, ${i / 100})`;
+			header.style.background = "#e0e0e0";
+			header.style.opacity = `${i / 100 - 0.05}`;
+			if (i >= 100) {
+				window.cancelAnimationFrame(animation_id);
+				is_incomplete = false;
+			    i = 0;
+			    return;
+			}
+		    animation_id = window.requestAnimationFrame(animate_to_light);
+
+		}
+
+		/*
+		  Event listener for  themebutton click
 	
-	if (!darkmode)
-	{
-	    header.style.boxShadow = "8px 8px 16px black";
-	    b =  setInterval(animate_to_dark,10);
-	    darkmode = true;
-	 
+		*/
+
+		themeButton.addEventListener("click", () => {
+			if (is_incomplete) {
+				window.cancelAnimationFrame(animation_id);
+				is_incomplete = false;
+				header.style.opacity = '0.95';
+				themeButton.style.opacity = '1';
+				i = 0;
+			}
+
+			if (!darkmode) {
+				header.style.boxShadow = "8px 8px -8px -8px black";
+				animation_id = window.requestAnimationFrame(animate_to_dark);
+				header.style.border = "inset #02e2e6 1px";
+				darkmode = true;
+
+			}
+			else {
+				header.style.boxShadow = "8px 8px -8px -8px #ffffff";
+				header.style.borderWidth = "0px";
+				themeButton.style.filter = "invert(0%)";
+				animation_id = window.requestAnimationFrame(animate_to_light);
+				darkmode = false;
+			}
+		});
 	}
-	else
-	{
-	    header.style.boxShadow = "8px 8px 16px #bababa, -8px -8px 16px #ffffff";
-	    b =  setInterval(animate_to_light,10);
-	    darkmode = false;
+
+	/*
+	
+	  Event listerner for menuIcon
+	
+	*/
+/*
+    {
+
+
+	let i = 0, animation_id = 0;
+	let is_incomplete = false;
+
+
+/*	function blur() {
+	    i += 6;
+	    header.style.filter = `blur(${i/30}px)`;
+	    main[0].style.filter = `blur(${i/30}px)`;
+	    if (i >=72)
+	    {
+		window.cancelAnimationFrame(animation_id);
+		is_incomplete = false;
+		header.style.opacity = '1';
+		header.style.background = "#e0e0e0";
+		return;
+	    }
+	   animation_id =  requestAnimationFrame(blur);
+
 	}
-    });
+
+	    function unblur()
+	{
+	    i -=6;
+		header.style.filter = `blur(${i/30}px)`;
+		main[0].style.filter = `blur(${i/30}px)`;
+		if (i <= 0)
+		{
+		    window.cancelAnimationFrame(animation_id);
+		    is_incomplete = false;
+		    header.style.opacity = '1';
+0		    header.style.background = "#e0e0e0";
+		    return;
+		}
+	    animation_id =  requestAnimationFrame(unblur);
+	    }
+
+
+		let is_menu_open = false;
+
+	menuIcon.addEventListener("click", () => {
+	    if (is_incomplete) {
+		window.cancelAnimationFrame(animation_id);
+		is_incomplete = false;
+		is_menu_open ? i = 72 : i = 0 ;
+	    }
+			if (is_menu_open) {
+			    requestAnimationFrame(unblur);
+				is_menu_open = false;
+			}
+	    else {
+	
+				menu.style.display = 'block';
+		requestAnimationFrame(blur);
+		header.style.backgroundColor = '#e0e0e0;'
+				is_menu_open = true;
+	    }
+	  
+		});
+
+
+	}*/
 }
+
